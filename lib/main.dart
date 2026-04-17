5a500b50e858458f068345be92cb446d1b6d62b0
Daiyan Abrar Samin <daiyan03samin@gmail.com>
2026-04-17 18:55:52 -0400

    All Initial Screens has been completed

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Local Heroes',
       debugShowCheckedModeBanner: false,
-      home: const FindHeroesScreen(),
+      home: Splashscreen(),
     );
   }
 }
